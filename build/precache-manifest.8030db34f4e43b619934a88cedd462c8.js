9dc23aa85be8ced89d2beb5641222babadd114ad
pesh12 <ekocibar@hotmail.com>
2019-06-17 18:22:26 +0200

    Build  for deployment

---
+++ build/precache-manifest.8030db34f4e43b619934a88cedd462c8.js
@@ -0,0 +1,46 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "5e2281059a2a3a20afae86d3dba82a2d",
+    "url": "/index.html"
+  },
+  {
+    "revision": "07b4e93164355e75aa32",
+    "url": "/static/css/2.266e55a5.chunk.css"
+  },
+  {
+    "revision": "ed25d345df9ad2c00240",
+    "url": "/static/css/main.916bbbe7.chunk.css"
+  },
+  {
+    "revision": "07b4e93164355e75aa32",
+    "url": "/static/js/2.8362a41e.chunk.js"
+  },
+  {
+    "revision": "ed25d345df9ad2c00240",
+    "url": "/static/js/main.0dd2eee4.chunk.js"
+  },
+  {
+    "revision": "42ac5946195a7306e2a5",
+    "url": "/static/js/runtime~main.a8a9905a.js"
+  },
+  {
+    "revision": "243379ccddf2f8d8467c0ecdb1761ea9",
+    "url": "/static/media/healthy.243379cc.jpg"
+  },
+  {
+    "revision": "1eaef50431926597d6c8c3110dc2938f",
+    "url": "/static/media/healty-square.1eaef504.jpg"
+  },
+  {
+    "revision": "30948c990f561bc5e7cb755ada03786b",
+    "url": "/static/media/honey.30948c99.jpeg"
+  },
+  {
+    "revision": "d38b848399d29d4cfaea4a04f8024579",
+    "url": "/static/media/olive.d38b8483.jpg"
+  },
+  {
+    "revision": "26b8ee04e9e91d4d4d36787294a70db5",
+    "url": "/static/media/sarma.26b8ee04.jpg"
+  }
+]);
