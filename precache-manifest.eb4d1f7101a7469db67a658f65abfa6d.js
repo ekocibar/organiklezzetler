177da16d8b6cae0262f8436e07bd42841efd4597
root <root@DESKTOP-OCOQRP6.localdomain>
2019-06-17 23:14:13 +0200

    Updates

---
+++ precache-manifest.eb4d1f7101a7469db67a658f65abfa6d.js
@@ -0,0 +1,46 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "977ee870c2a3bea98f9c78ae68304a8e",
+    "url": "/organiklezzetler/index.html"
+  },
+  {
+    "revision": "07b4e93164355e75aa32",
+    "url": "/organiklezzetler/static/css/2.266e55a5.chunk.css"
+  },
+  {
+    "revision": "1a2bd47fccdd321e756c",
+    "url": "/organiklezzetler/static/css/main.35f05ef8.chunk.css"
+  },
+  {
+    "revision": "07b4e93164355e75aa32",
+    "url": "/organiklezzetler/static/js/2.8362a41e.chunk.js"
+  },
+  {
+    "revision": "1a2bd47fccdd321e756c",
+    "url": "/organiklezzetler/static/js/main.05a96cb1.chunk.js"
+  },
+  {
+    "revision": "6ec2b6b3e5412d59019b",
+    "url": "/organiklezzetler/static/js/runtime~main.aa9a8f56.js"
+  },
+  {
+    "revision": "243379ccddf2f8d8467c0ecdb1761ea9",
+    "url": "/organiklezzetler/static/media/healthy.243379cc.jpg"
+  },
+  {
+    "revision": "1eaef50431926597d6c8c3110dc2938f",
+    "url": "/organiklezzetler/static/media/healty-square.1eaef504.jpg"
+  },
+  {
+    "revision": "30948c990f561bc5e7cb755ada03786b",
+    "url": "/organiklezzetler/static/media/honey.30948c99.jpeg"
+  },
+  {
+    "revision": "d38b848399d29d4cfaea4a04f8024579",
+    "url": "/organiklezzetler/static/media/olive.d38b8483.jpg"
+  },
+  {
+    "revision": "26b8ee04e9e91d4d4d36787294a70db5",
+    "url": "/organiklezzetler/static/media/sarma.26b8ee04.jpg"
+  }
+]);
